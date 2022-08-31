bd46830a670ddb30055824de55cbe86ce1f59a80
Sarah Guillon <sarah.guillon.11@gmail.com>
2022-08-31 15:24:49 +0200

    sales-form

--- src/components/salesForm.jsx
+++ src/components/salesForm.jsx
@@ -33,57 +33,59 @@ const SalesForm = () => {
 
   return (
     <div className="sales-form-card">
-      <h2 className="header-form-card">Add a new sale :</h2>
-      <div className="sales-form">
-        <div className="input-container">
-          <input
-            placeholder="Product name"
-            type="text"
-            name="product"
-            required
-            id="productName"
-            value={productName}
-            onChange={(event)=>setProductName(event.target.value)}
-          />
+      <div className="sales-form-card-width">
+        <h2 className="header-form-card"> Add new Sales</h2>
+        <div className="sales-form">
+          <div className="input-container product-input">
+            <input
+              placeholder="Product name"
+              type="text"
+              name="product"
+              required
+              id="productName"
+              value={productName}
+              onChange={(event)=>setProductName(event.target.value)}
+            />
+          </div>
+          <div className="input-container">
+            <input
+              placeholder="Quantity"
+              type="number"
+              inputmode="numeric"
+              name="quantity"
+              required
+              id="quantity"
+              value={quantity}
+              onChange={(event)=>setQuantity(event.target.value)}
+            />
+          </div>
+          <div className="input-container">
+            <input
+              placeholder="Price"
+              type="number"
+              name="unitPrice"
+              required
+              id="unitPrice"
+              value={unitPrice}
+              onChange={(event)=>setUnitPrice(event.target.value)}
+            />
+            <span> ₹ / unit</span>
+          </div>
+          <div className="input-container">
+            <input
+              placeholder="Total Price"
+              type="number"
+              name="totalPrice"
+              required
+              id="totalPrice"
+              value={totalPrice}
+              onClick={(event)=>setTotalPrice(unitPrice * quantity)}
+              onChange={(event)=>setTotalPrice(event.target.value)}
+            />
+            <span> ₹ </span>
+          </div>
+          <button className="sales-button" onClick={e => handleSubmit(e)}> + </button>
         </div>
-        <div className="input-container">
-          <input
-            placeholder="Quantity"
-            type="number"
-            inputmode="numeric"
-            name="quantity"
-            required
-            id="quantity"
-            value={quantity}
-            onChange={(event)=>setQuantity(event.target.value)}
-          />
-        </div>
-        <div className="input-container">
-          <input
-            placeholder="Price"
-            type="number"
-            name="unitPrice"
-            required
-            id="unitPrice"
-            value={unitPrice}
-            onChange={(event)=>setUnitPrice(event.target.value)}
-          />
-          <span> ₹ / unit</span>
-        </div>
-        <div className="input-container">
-          <input
-            placeholder="Total Price"
-            type="number"
-            name="totalPrice"
-            required
-            id="totalPrice"
-            value={totalPrice}
-            onClick={(event)=>setTotalPrice(unitPrice * quantity)}
-            onChange={(event)=>setTotalPrice(event.target.value)}
-          />
-          <span> ₹ </span>
-        </div>
-        <button className="sales-button" onClick={e => handleSubmit(e)}> + </button>
       </div>
       {/* <div><p> discount applied : {totalPrice - unitPrice * quantity} ₹</p></div> */}
     </div>
